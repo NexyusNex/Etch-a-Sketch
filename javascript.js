0be7a72f465f138f0ddf1fe08093514a2f9bf1d5
NexyusNex <shootdietale@gmail.com>
2022-11-08 14:58:03 +0100

    Added a function that adds 16x16 square shaped divs

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+for(var i = 0; i<16; i++){
+    for(var j = 0; j<16; j++){
+        const div = document.createElement('div');
+        div.classList.add("square");
+        container.appendChild(div);
+    }
+}
