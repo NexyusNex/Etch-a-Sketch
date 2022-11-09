542d5f700d3d8aaaba1e4a5fca6d37d736fc4f45
NexyusNex <shootdietale@gmail.com>
2022-11-09 13:37:27 +0100

    Grids now change color on mouse hover

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 const container = document.querySelector(".container");
-for(var i = 0; i<16; i++){
-    for(var j = 0; j<16; j++){
+for(var i = 0; i<2; i++){
+    for(var j = 0; j<2; j++){
         const div = document.createElement('div');
         div.classList.add("square");
+        div.setAttribute("id","sqid");
         container.appendChild(div);
     }
 }
+const divs = document.querySelectorAll('.square');
+divs.forEach(div =>{
+    div.addEventListener('mouseover',function(){
+        div.style.backgroundColor='blue';
+    })
+    }     
+)
