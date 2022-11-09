607fd7b4e20852c817d5f3da26dfe30dc5a3d0c0
NexyusNex <shootdietale@gmail.com>
2022-11-09 15:03:44 +0100

    the user can now change size of the grid

--- javascript.js
+++ javascript.js
@@ -1,22 +1,36 @@
 let size = 2;
 const container = document.querySelector(".container");
-container.setAttribute('style',`grid-template-columns: repeat(${size},1fr); grid-template-columns: repeat(${size},1fr);`);
-
-for(var i = 0; i<size; i++){
-    for(var j = 0; j<size; j++){
-        const div = document.createElement('div');
-        div.classList.add("square");
-        div.setAttribute("id","sqid");
-        container.appendChild(div);
-    }
+function removeGrid(){
+        const divs = document.querySelectorAll('.square');
+        divs.forEach(div =>{
+            div.remove();
+        })
 }
-const divs = document.querySelectorAll('.square');
-divs.forEach(div =>{
-    div.addEventListener('mouseover',function(){
-        div.style.backgroundColor='blue';
-    })
-    }     
+function createGrid(){
+        
+        container.setAttribute('style',`grid-template-columns: repeat(${size},1fr); grid-template-rows: repeat(${size},1fr);`);
+
+        for(var i = 0; i<size; i++){
+            for(var j = 0; j<size; j++){
+                const div = document.createElement('div');
+                div.classList.add("square");
+                container.appendChild(div);
+            }
+        }
+        const divs = document.querySelectorAll('.square');
+        divs.forEach(div =>{
+        div.addEventListener('mouseover',function(){
+            div.style.backgroundColor='black';
+        })
+        }     
 )
+}
+
+createGrid();
 function start(){
-    size = prompt('Enter the grid size number!',"Enter here");
+    do{
+    size = prompt('Enter the grid size number!(Max is 100)',"Enter here");
+    }while(size<0 || size>100 || isNaN(size)==true);
+    removeGrid();
+    createGrid();
 }
